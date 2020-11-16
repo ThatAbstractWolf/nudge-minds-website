610a794a33ed0ff31c1759a75b7afb7ee2367204
Jake Brown <Jake@abstract-studios.co.uk>
2020-11-16 23:34:46 +0000

    Adding initial setup of CraftCMS 3 and Gulp for SCSS and JS building and minification.

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+
+const sass = require('gulp-sass');
+
+const concat = require('gulp-concat');
+
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
+
+const del = require('del');
+ 
+ gulp.task('js', () => {
+    return gulp.src('assets/scripts/*.js')
+		.pipe(uglify())
+		.pipe(concat('main.js'))
+        .pipe(gulp.dest('./web/dist/'));
+});
+ 
+ gulp.task('css', () => {
+    return gulp.src('assets/styles/*.scss')
+        .pipe(sass().on('error', sass.logError))
+		.pipe(cleanCSS())
+		.pipe(concat('main.css'))
+        .pipe(gulp.dest('./web/dist/'));
+});
+
+gulp.task('watch', () => {
+	
+    gulp.watch('assets/styles/*.scss', (done) => {
+        gulp.series(['css'])(done);
+    });
+	
+	gulp.watch('assets/scripts/*.js', (done) => {
+        gulp.series(['js'])(done);
+    });
+});
+
+gulp.task('default', gulp.series(['css', 'js']));
